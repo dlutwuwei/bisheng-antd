6331a3bb0159a0f0024c44185737f8c0041917e3
wuwei <wuwei@bytedance.com>
2017-09-05 18:13:27 +0800

    fisrt commit

---
+++ bisheng.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const CSSSplitWebpackPlugin = require('css-split-webpack-plugin').default;
+
+module.exports = {
+  port: 8000,
+  // 扫描docs和posts中所有markdown
+  source: {
+    components: 'components',
+    docs: 'docs'
+  },
+  theme: './theme',
+  htmlTemplate: './theme/static/template.html',
+  themeConfig: {
+    categoryOrder: {
+      设计原则: 2,
+      Principles: 2,
+    },
+    typeOrder: {
+      General: 0,
+      Layout: 1,
+      Navigation: 2,
+      'Data Entry': 3,
+      'Data Display': 4,
+      Feedback: 5,
+      Localization: 6,
+      Other: 7,
+    },
+    docVersions: {
+      '0.9.x': 'http://09x.ant.design',
+      '0.10.x': 'http://010x.ant.design',
+      '0.11.x': 'http://011x.ant.design',
+      '0.12.x': 'http://012x.ant.design',
+      '1.x': 'http://1x.ant.design',
+    },
+  },
+  entryName: 'index',
+  filePathMapper(filePath) {
+    if (filePath === '/index.html') {
+      return ['/index.html', '/index-cn.html'];
+    }
+    if (filePath.endsWith('/index.html')) {
+      return [filePath, filePath.replace(/\/index\.html$/, '-cn/index.html')];
+    }
+    if (filePath !== '/404.html' && filePath !== '/index-cn.html') {
+      return [filePath, filePath.replace(/\.html$/, '-cn.html')];
+    }
+    return filePath;
+  },
+  doraConfig: {
+    verbose: true,
+    plugins: ['dora-plugin-upload'],
+  },
+  webpackConfig(config) {
+
+    config.externals = {
+      'react-router-dom': 'ReactRouterDOM',
+    };
+
+    config.babel.plugins.push([
+      require.resolve('babel-plugin-transform-runtime'),
+      {
+        polyfill: false,
+        regenerator: true,
+      },
+    ]);
+
+    // config.plugins.push(new CSSSplitWebpackPlugin({ size: 2000 }));
+
+    return config;
+  },
+};
---
+++ theme/index.js
@@ -0,0 +1,86 @@
+const path = require('path');
+
+const homeTmpl = './template/Home/index';
+const contentTmpl = './template/Content/index';
+
+function pickerGenerator(module) {
+  const tester = new RegExp(`^docs/${module}`);
+  return (markdownData) => {
+    const filename = markdownData.meta.filename;
+    if (tester.test(filename) &&
+        !/\/demo$/.test(path.dirname(filename))) {
+      return {
+        meta: markdownData.meta,
+      };
+    }
+  };
+}
+
+module.exports = {
+  lazyLoad(nodePath, nodeValue) {
+    if (typeof nodeValue === 'string') {
+      return true;
+    }
+    return nodePath.endsWith('/demo');
+  },
+  // 将文件分类到不同对象中，components, changlog，docs和其他
+  pick: {
+    components(markdownData) {
+      const filename = markdownData.meta.filename;
+      // 必须在components文件夹内，且不能已demo结尾
+      if (!/^components/.test(filename) ||
+          /[/\\]demo$/.test(path.dirname(filename))) return;
+
+      return {
+        meta: markdownData.meta,
+      };
+    },
+    changelog(markdownData) {
+      if (/CHANGELOG/.test(markdownData.meta.filename)) {
+        return {
+          meta: markdownData.meta,
+        };
+      }
+    },
+    'docs/pattern': pickerGenerator('pattern'),
+    'docs/react': pickerGenerator('react'),
+    'docs/resource': pickerGenerator('resource'),
+    'docs/spec': pickerGenerator('spec'),
+  },
+  plugins: [
+    'bisheng-plugin-description',
+    'bisheng-plugin-toc?maxDepth=2&keepElem',
+    'bisheng-plugin-antd',
+    'bisheng-plugin-react?lang=__react',
+  ],
+  routes: {
+    path: '/',
+    component: './template/Layout/index',
+    indexRoute: { component: homeTmpl },
+    childRoutes: [{
+      path: 'index-cn',
+      component: homeTmpl,
+    }, {
+      path: 'docs/pattern/:children',
+      component: contentTmpl,
+    }, {
+      path: 'docs/react/:children',
+      component: contentTmpl,
+    }, {
+      path: 'changelog',
+      component: contentTmpl,
+    }, {
+      path: 'changelog-cn',
+      component: contentTmpl,
+    }, {
+      path: 'components/:children/',
+      component: contentTmpl,
+    }, {
+      path: 'docs/spec/:children',
+      component: contentTmpl,
+    }, {
+      path: 'docs/resource/:children',
+      component: contentTmpl,
+    }],
+  },
+};
